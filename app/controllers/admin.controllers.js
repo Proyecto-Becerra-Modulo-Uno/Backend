99957788f5c79e2ec9c3b6ae1dd5869cfa81b968
FREDDYGF <fregiraldo1@misena.edu.co>
2024-10-30 20:48:26 -0500

    integracion con la rama de agustin

--- app/controllers/admin.controllers.js
+++ app/controllers/admin.controllers.js
@@ -87,6 +87,7 @@ export const listarSesiones = async (req, res) => {
     }
 };
 
+
 export const registroInicioSesion = async (req, res) => {
 
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
